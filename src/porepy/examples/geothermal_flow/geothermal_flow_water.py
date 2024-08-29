1988f99ef5aeca5ede3f85ead1c74b868f03b50f
Omar Duran <omaryesiduran@gmail.com>
2024-08-29 16:10:04 +0200

    WIP: New setting

--- src/porepy/examples/geothermal_flow/geothermal_flow_water.py
+++ src/porepy/examples/geothermal_flow/geothermal_flow_water.py
@@ -20,8 +20,8 @@ s_tol = 10.0
 day = 86400 #seconds in a day.
 year = 365.0 * day
 tf = 2000.0 * year # final time [2000 years]
-dt = 2000.0 * year # time step size [2000 years]
-# dt = (2000.0/1200.0) * year # time step size [1.666 years]
+# dt = 2000.0 * year # time step size [2000 years]
+dt = (2000.0/1200.0) * year # time step size [1.666 years]
 time_manager = pp.TimeManager(
     schedule=[0.0, tf],
     dt_init=dt,
@@ -49,7 +49,7 @@ params = {
     "reduce_linear_system_q": False,
     "nl_convergence_tol": np.inf,
     "nl_convergence_mass_tol_res": s_tol * 1.0e-5,
-    "nl_convergence_energy_tol_res": s_tol * 1.0e-5,
+    "nl_convergence_energy_tol_res": s_tol * 1.0e-3,
     "nl_convergence_temperature_tol_res": s_tol * 1.0e-3,
     "nl_convergence_fractions_tol_res": s_tol * 1.0e-3,
     "max_iterations": 100,
@@ -935,10 +935,10 @@ def draw_and_save_comparison(T_proj,T_vtk,S_proj,S_vtk,H_proj,H_vtk):
 draw_and_save_comparison(T_proj,T_vtk,S_proj,S_vtk,H_proj,H_vtk)
 
 # project solution as initial guess
-x = model.equation_system.get_variable_values(iterate_index=0).copy()
-delta_x = model.increment_from_projected_solution()
-x_k = x + delta_x
-model.equation_system.set_variable_values(values=x_k, iterate_index=0)
+# x = model.equation_system.get_variable_values(iterate_index=0).copy()
+# delta_x = model.increment_from_projected_solution()
+# x_k = x + delta_x
+# model.equation_system.set_variable_values(values=x_k, iterate_index=0)
 
 # assert False
 # print geometry
